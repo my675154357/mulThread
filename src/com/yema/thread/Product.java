fe258c281a40cbb856e4c47ff21dd41f91731d49
yema <a.mye@foxmail.com>
2018-03-05 18:09:57 +0800

    实践使用git管理项目。初始导入mulThread工程

---
+++ src/com/yema/thread/Product.java
@@ -0,0 +1,11 @@
+package com.yema.thread;
+
+public class Product {
+	//定义产品的唯一ID
+    int id;
+    //定义构造方法初始化产品id
+    public Product(int id) {
+        this.id=id;
+        // TODO Auto-generated constructor stub
+    }
+}
